e3680c599bd5fa5c80e76f8375f5383fab23d5cf
Jarek Plonski <jarek.github@gmail.com>
2014-09-07 14:33:17 +0200

    version bump

--- lib/hacklab_decorator/railtie.rb
+++ lib/hacklab_decorator/railtie.rb
@@ -1,22 +1,23 @@
 module HacklabDecorator
 
   class Railtie < Rails::Railtie
+    config.after_initialize do |app|
+      app.config.paths.add 'app/decorators', eager_load: true
+    end
+
     initializer "hacklab_decorator.setup_orm" do |app|
       ActiveSupport.on_load :active_record do
         HacklabDecorator.setup_orm self
       end
     end
 
-    initialize 'hacklab_decorator.setup_action_controller' do |app|
+    initializer 'hacklab_decorator.setup_action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         HacklabDecorator.setup_action_controller self
       end
     end
 
 
-    config.after_initialize do |app|
-      app.config.paths.add 'app/decorators', eager_load: true
-    end
   end
 
 end
