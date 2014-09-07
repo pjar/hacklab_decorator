2dbda6af9cf5e626eea4402c178a7793f9cfc569
Michał Samluk <michal@codequest.eu>
2014-09-07 09:53:52 +0200

    Fix

--- lib/hacklab_decorator/railtie.rb
+++ lib/hacklab_decorator/railtie.rb
@@ -1,7 +1,7 @@
 module HacklabDecorator
 
   class Railtie < Rails::Railtie
-    initializer "draper.setup_orm" do |app|
+    initializer "hacklab_decorator.setup_orm" do |app|
       ActiveSupport.on_load :active_record do
         HacklabDecorator.setup_orm self
       end
